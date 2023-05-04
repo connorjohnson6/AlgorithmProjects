fadb670fc5ca34500f05879e442920fe4fb182c3
connorjohnson6 <70149458+connorjohnson6@users.noreply.github.com>
2023-05-04 15:19:25 -0400

    Edits for changes in Linked-List

--- Lab1/Lab1/mainProgram.java
+++ Lab1/Lab1/mainProgram.java
@@ -2,28 +2,34 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
-import singlyLinkedList.singlyLinkedList;
+import sortingAlgorithms.sortingAlgorithms;
+import sortingAlgorithms.sortingAlgorithms.Stack;
+import sortingAlgorithms.sortingAlgorithms.Queue;
+
 public class mainProgram {
 
     public static void main(String[] args) {
         try {
-            File file = new File("/Users/Johnson_code/CJohnson-435/CJohnson-435/Lab1/textFiles/magicitems.txt");
+            File file = new File("/Users/Johnson_code/CJohnson-435-1/Lab2/textFiles/magicitems.txt");
             Scanner scanner = new Scanner(file);
-            
+
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String originalLine = line;  // store the original line with spaces to later print
 
                 line = line.replaceAll("[^a-zA-Z0-9]", "").toLowerCase(); // remove all non-alphanumeric characters and convert to lowercase(chatgpt suggested code)
-                singlyLinkedList sStack = new singlyLinkedList();
-                singlyLinkedList sQueue = new singlyLinkedList();
-            
+                sortingAlgorithms sa = new sortingAlgorithms();
+
+                Stack sStack = sa.new Stack();
+                Queue sQueue = sa.new Queue();
+
                 // Push each character on the stack and enqueue each character in a queue
                 for (char c : line.toCharArray()) {
                     sStack.push(c);
                     sQueue.enqueue(c);
                 }
-            
+
+
                 boolean truePal = true;
                 while (!sStack.isEmpty() && !sQueue.isEmpty()) { //make sure neither are empty
 
@@ -36,12 +42,12 @@ public class mainProgram {
                         break;
                     }
                 }
-            
+
                 if (truePal) {
                     System.out.println(originalLine);  // print the original line with spaces
                 }
             }
-            
+
 
 
             scanner.close(); 
@@ -49,5 +55,5 @@ public class mainProgram {
             e.printStackTrace();
         }
     } //end of mainProgram
-    
+
 }//end of mainProgram
